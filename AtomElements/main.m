a952346dfedcf9f79cbfce195cf5a68f76e1107b
icetusk <my@mail>
2017-01-06 23:04:06 +0300

    Initial Commit

---
+++ AtomElements/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  AtomElements
+//
+//  Created by icetusk on 06/01/2017.
+//  Copyright © 2017 icetusk. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
